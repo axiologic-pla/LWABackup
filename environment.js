0e067216d0615855b12201f9f5fa7a2bf52c35ed
adrian.ganga <adiganga2002@gmail.com>
2023-06-12 15:28:54 +0300

    update with last changes

--- environment.js
+++ environment.js
@@ -1,6 +1,10 @@
 export default {
-  "epiDomain": "demo.epi",
+  "epiDomain": "local.epi",
   "epiProtocolVersion": 1,
-  "appBuildVersion": "2023.0.5",
-  "bdnsUrl": "https://raw.githubusercontent.com/PharmaLedger-IMI/mobile-bdns/master/bdns.json"
+  "appBuildVersion": "2023.0.4",
+  "timePerCall": 10000,
+  "totalWaitTime": 60000,
+  "gto_TimePerCall": 3000,
+  "gto_TotalWaitTime": 15000,
+  "bdnsUrl": "http://127.0.0.1:8080/bdns"
 }
